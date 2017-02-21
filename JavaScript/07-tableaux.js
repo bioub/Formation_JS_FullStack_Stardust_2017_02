79d0082312641af131f8598e11d67f1cf3a66528
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-02-21 17:38:17 +0100

    Jour 2

---
+++ JavaScript/07-tableaux.js
@@ -0,0 +1,32 @@
+// Tableau (en anglais array)
+// Un type complexe qui permet de contenir plusieurs valeur
+
+var prenoms = ['Romain', 'Brian', 'Alexis'];
+
+console.log(prenoms[0]); // Romain
+console.log(prenoms[3]); // undefined
+
+console.log(prenoms[prenoms.length - 1]); // Alexis
+
+prenoms.push('Khaled');
+var pos = prenoms.indexOf('Brian');
+console.log(pos); // 1
+console.log(prenoms[pos]); // Brian
+
+// Pour supprimer du tableau
+prenoms.splice(pos, 1);
+
+for (var i=0; i<prenoms.length; i++) {
+  console.log(prenoms[i]);
+}
+
+prenoms.forEach(function appelleMoiSurChaqueElementDuTableau(prenom, i) {
+  console.log(prenom);
+});
+
+// array.indexOf() => indexOf(array, )
+// array.includes() => includes(array, 3)
+// array.slice() => slice(array)
+// reverse
+// find
+
