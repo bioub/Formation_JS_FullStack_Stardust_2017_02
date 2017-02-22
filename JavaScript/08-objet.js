64a2b748c70e142e3d3c30fde0ddfa471b4b3fe4
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-02-22 14:25:56 +0100

    Fin du chapitre JS

---
+++ JavaScript/08-objet.js
@@ -0,0 +1,38 @@
+// Objet (en JS)
+// Comme les tableaux, un objet est un type complexe
+// qui permet de stocker plusieurs valeurs
+// La différence, pour retrouver une valeur dans un tableau
+// il faut connaitre la position/l'indice (number)
+// Dans un objet il faut connaitre la clé (string)
+
+// Exemple, ensemble de capitales
+// qu'on retrouve à partir du nom du pays
+
+var capitales = {
+  france: 'Paris',
+  espagne: 'Madrid',
+  allemagne: 'Berlin'
+};
+
+// Pour accéder à une valeur on peut utiliser les []
+// comme pour les tableaux
+console.log(capitales['france']);
+
+// ou en général avec le .
+console.log(capitales.france);
+
+// En JS, la plupart du temps on ne fait que manipuler
+// des objets existant, exemples :
+console.log(typeof console); // object
+console.log(typeof Math); // object
+
+// Parfois il faut créer un objet d'un certain type
+var maintenant = new Date();
+console.log(typeof maintenant); // object
+
+// L'objet maintenant contient la méthode getHours
+// (une méthode est une fonction dans un objet)
+// (une propriété est quelque chose dans un objet
+// qui n'est pas une fonction)
+console.log(maintenant.getHours()); // 14
+
--- JavaScript/ex-jeuduplusoumoins.js
+++ JavaScript/ex-jeuduplusoumoins.js
@@ -1,21 +1,65 @@
+// Importe l'API readline qui permet de poser la question
 var readline = require('readline');
 
+// Configure readline pour la question s'affiche dans la console (process.stdout)
+// et que la réponse soit lue sur le clavier (process.stdin)
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
+// Génère l'entier aléatoire entre 0 et 100
+var entierAlea = Math.floor(Math.random() * 101);
+
+// Créé le tableau pour stocker les essais
+var essais = [];
+
+// La fonction jouer permet de poser la question et obtenir
+// la réponse, rappeler jouer permet de rejouer.
 var jouer = function() {
+  // Si le tableau n'est pas vide (0 converti en booléen === false, vrai sinon)  
+  // on affiche les précédents essais
+  if (essais.length) {
+    console.log('Vous avez déjà joué : ' + essais.join(', '));
+  }
 
-    rl.question('What do you think of Node.js? ', function answerCallback(answer) {
-      // TODO: Log the answer in a database
-      console.log(typeof answer);
+  // On pose la question et lorsqu'on appuiera sur ENTREE
+  // le callback sera appelé avec la réponse en paramètre d'entrée
+  rl.question('Saisir un nombre : ', function answerCallback(answer) {
 
-      jouer();
+    // answer est de type string, donc pour la comparer
+    // à entierSaisi de type number il faut convertir
+    var entierSaisi = parseInt(answer);
 
-      // rl.close();
-    });
+    // Si la conversion est impossible (saisie 'ABC')
+    // on affiche une erreur et on rejoue
+    if (isNaN(entierSaisi)) {
+      console.log('Erreur : il faut saisir un nombre !');
+      return jouer();
+    }
+
+    // On stocke le nombre à fin du tableau essais 
+    essais.push(entierSaisi);
+
+    // On rejoue si le nombre est trop petit
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      return jouer();
+    }
+
+    // On rejoue si le nombre est trop grand
+    if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      return jouer();
+    }
+
+    // On termine le programme s'il a trouvé
+    console.log('Gagné');
+    rl.close();
+  });
 
 };
 
+// Il faut appeler jouer au moins une fois ici pour poser la première
+// question
 jouer();
